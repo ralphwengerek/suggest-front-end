70fb115b254eda2c2119d1ca162581db1b2dce5c
Lyla Fibert <lylafibert@MacBook-Pro-6.local>
2018-11-27 16:31:52 +0000

    storing changes

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,20 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" >
+@import "~vue-material/dist/theme/engine"; // Import the theme engine
+
+@include md-register-theme(
+  "default",
+  (
+    primary: md-get-palette-color(red, A200),
+    // The primary color of your application
+      accent: md-get-palette-color(red, A200)// The accent or secondary color
+  )
+);
+
+@import "~vue-material/dist/theme/all"; // Apply the theme
+
 .md-app {
   min-height: 350px;
 }
