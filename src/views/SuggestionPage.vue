3d4a845448673bdc93c915efeab315c78dec338f
Lyla Fibert <lylafibert@MacBook-Pro-6.local>
2018-11-28 12:12:02 +0000

    added validation, stopped from submitting if invalid

--- src/views/SuggestionPage.vue
+++ src/views/SuggestionPage.vue
@@ -1,89 +1,126 @@
 <template>
-    <div>
-        <form novalidate class="md-layout" @submit.prevent="validateUser">
-            <md-card class="md-layout-item md-size-50 md-small-size-100">
-                <md-card-header>
-                    <div class="md-title">Suggest a course</div>
-                </md-card-header>
+  <div>
+    <form novalidate class="md-layout" @submit.prevent="validateUser" ng-submit="form.$valid">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">Suggest a course</div>
+        </md-card-header>
 
-                <md-card-content>
-                    <div class="md-layout md-gutter">
-                        <div class="md-layout-item md-small-size-100">
-                            <md-field :class="getValidationClass('courseTitle')">
-                                <label for="course-title">Course Title</label>
-                                <md-input name="course-title" id="course-title" v-model="form.courseName"
-                                          :disabled="sending"/>
-                                <span class="md-error"
-                                      v-if="!$v.form.courseName.required">a course title is required</span>
-                            </md-field>
-                        </div>
-                    </div>
+        <md-card-content>
+          <div class="md-layout md-gutter">
+            <div class="md-layout-item md-small-size-100">
+              <md-field :class="getValidationClass('courseName')">
+                <label for="course-name">Course Title</label>
+                <md-input
+                  name="course-name"
+                  id="course-name"
+                  v-model="form.courseName"
+                  :disabled="sending"
+                />
+                <span
+                  class="md-error"
+                  v-if="!$v.form.courseName.required"
+                >a course title is required</span>
+              </md-field>
+            </div>
+          </div>
 
+          <md-field :class="getValidationClass('courseDescription')">
+            <label for="description">Description</label>
+            <md-textarea
+              type="course-description"
+              name="course-description"
+              id="course-description"
+              v-model="form.courseDescription"
+              :disabled="sending"
+            />
+            <span
+              class="md-error"
+              v-if="!$v.form.courseDescription.required"
+            >The description is required</span>
+          </md-field>
 
-                    <md-field :class="getValidationClass('description')">
-                        <label for="description">Description</label>
-                        <md-textarea type="description" name="description" id="description" v-model="form.courseDescription"
-                                     :disabled="sending"/>
-                        <span class="md-error" v-if="!$v.form.courseDescription.required">The description is required</span>
-                        <span class="md-error" v-else-if="!$v.form.courseDescription.description">Invalid description</span>
-                    </md-field>
+          <div class="md-layout-item">
+            <md-field :class="getValidationClass('abilityLevelId')">
+              <md-select
+                v-model="form.abilityLevelId"
+                name="ability-level"
+                id="ability-level"
+                placeholder="Ability Level"
+              >
+                <md-option value="1">Beginner</md-option>
+                <md-option value="2">Intermediate</md-option>
+                <md-option value="3">Advanced</md-option>
+              </md-select>
+              <span
+                class="md-error"
+                v-if="!$v.form.abilityLevelId.required"
+              >Please enter the ability level</span>
+            </md-field>
+          </div>
 
-                    <div class="md-layout-item">
-                        <md-field>
-                            <md-select v-model="form.abilityLevelId" name="ability-level" id="ability-level"
-                                       placeholder="Ability Level">
-                                <md-option value="1">Beginner</md-option>
-                                <md-option value="2">Intermediate</md-option>
-                                <md-option value="3">Advanced</md-option>
-                            </md-select>
-                        </md-field>
-                    </div>
+          <md-checkbox v-model="runCourse">I want to run this course</md-checkbox>
 
-                    <md-checkbox v-model="runCourse">I want to run this course</md-checkbox>
+          <div id="host-details" v-if="runCourse">
+            <div class="md-layout md-gutter">
+              <div class="md-layout-item">
+                <md-field :class="getValidationClass('authorName')">
+                  <label for="author-name">Author Name</label>
+                  <md-input
+                    name="author-name"
+                    id="author-name"
+                    v-model="form.authorName"
+                    :disabled="sending"
+                  />
+                </md-field>
+              </div>
 
-                    <div id="host-details" v-if="runCourse">
+              <div class="md-layout-item">
+                <md-field>
+                  <md-select
+                    v-model="form.authorRole"
+                    name="author-role"
+                    id="author-role"
+                    placeholder="Author Role"
+                  >
+                    <md-option value="roleDev">Product Developer</md-option>
+                  </md-select>
+                </md-field>
+              </div>
 
-                        <div class="md-layout md-gutter">
-                            <div class="md-layout-item">
-                                <md-field :class="getValidationClass('authorName')">
-                                    <label for="author-name">Author Name</label>
-                                    <md-input name="author-name" id="author-name" v-model="form.authorName"
-                                              :disabled="sending"/>
-                                </md-field>
-                            </div>
+              <div class="md-layout-item">
+                <md-field>
+                  <md-select
+                    v-model="form.authorLevel"
+                    name="author-level"
+                    id="author-level"
+                    placeholder="Author Level"
+                  >
+                    <md-option value="levelOne">Level 1</md-option>
+                  </md-select>
+                </md-field>
+              </div>
+            </div>
+          </div>
+        </md-card-content>
 
-                            <div class="md-layout-item">
-                                <md-field>
-                                    <md-select v-model="form.authorRole" name="author-role" id="author-role"
-                                               placeholder="Author Role">
-                                        <md-option value="roleDev">Product Developer</md-option>
-                                    </md-select>
-                                </md-field>
-                            </div>
+        <md-progress-bar md-mode="indeterminate" v-if="sending"/>
+        <md-card-actions>
+          <md-button
+            type="submit"
+            class="md-primary"
+            v-on:click="submitForm"
+            :disabled="sending"
+          >Suggest Course</md-button>
+        </md-card-actions>
+      </md-card>
 
-                            <div class="md-layout-item">
-                                <md-field>
-                                    <md-select v-model="form.authorLevel" name="author-level" id="author-level"
-                                               placeholder="Author Level">
-                                        <md-option value="levelOne">Level 1</md-option>
-                                    </md-select>
-                                </md-field>
-                            </div>
-                        </div>
-                    </div>
-                </md-card-content>
-
-                <md-progress-bar md-mode="indeterminate" v-if="sending"/>
-                <md-card-actions>
-                    <md-button type="submit" class="md-primary"  v-on:click="submitForm" :disabled="sending">Suggest Course</md-button>
-                </md-card-actions>
-            </md-card>
-
-            <md-snackbar :md-active.sync="suggestionAdded">The suggestion "{{courseSuggestion}}" was successfully
-                submitted!
-            </md-snackbar>
-        </form>
-    </div>
+      <md-snackbar :md-active.sync="suggestionAdded">
+        The suggestion "{{courseSuggestion}}" was successfully
+        submitted!
+      </md-snackbar>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -118,23 +155,29 @@ export default {
 
       courseDescription: {
         required
+      },
+
+      abilityLevelId: {
+        required
       }
     }
   },
 
   methods: {
     submitForm() {
-      console.log("submitting: ", this.form);
-      this.$http
-        .post("http://localhost:5000/api/suggestions", this.form, {
-          headers: { "Content-Type": "application/json;charset=utf-8" }
-        })
-        .then(response => {
-          console.log(response);
-        })
-        .catch(err => {
-          console.log(err);
-        });
+      if (this.$v.form.$valid) {
+        console.log("submitting: ", this.form);
+        this.$http
+          .post("http://localhost:5000/api/suggestions", this.form, {
+            headers: { "Content-Type": "application/json;charset=utf-8" }
+          })
+          .then(response => {
+            console.log(response);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
     },
 
     getValidationClass(fieldName) {
