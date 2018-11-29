59f74a347d9d66bf332655879144a8d66f22c88b
Lyla Fibert <lylafibert@MacBook-Pro-6.local>
2018-11-29 10:47:03 +0000

    centred form

--- src/views/SuggestionPage.vue
+++ src/views/SuggestionPage.vue
@@ -1,150 +1,133 @@
 <template>
-  <div>
-    <form
-      novalidate
-      class="md-layout"
-      @submit.prevent="validateForm"
-    >
-      <md-card class="md-layout-item md-size-50 md-small-size-100">
-        <md-card-header>
-          <div class="md-title">Suggest a course</div>
-        </md-card-header>
+  <div >
+    <form novalidate @submit.prevent="validateForm">
+      <div class="md-layout md-alignment-top-center">
+        <div class="md-layout-item md-large-size-50 md-small-size-100">
+        <md-card>
+          <md-card-header  class="center">
+            <div class="md-title ">Suggest a course</div>
+          </md-card-header>
 
-        <md-card-content>
-          <div class="md-layout md-gutter">
-            <div class="md-layout-item md-small-size-100">
-              <md-field :class="getValidationClass('courseName')">
-                <label for="course-name">Course Title</label>
-                <md-input
-                  name="courseName"
-                  id="course-name"
-                  v-model="form.courseName"
-                  :disabled="sending"
-                />
-                <span
-                  class="md-error"
-                  v-if="!$v.form.courseName.required"
-                >A course title is required</span>
-              </md-field>
-            </div>
-          </div>
-
-          <md-field :class="getValidationClass('courseDescription')">
-            <label for="course-description">Description</label>
-            <md-textarea
-              type="course-description"
-              name="courseDescription"
-              id="course-description"
-              v-model="form.courseDescription"
-              :disabled="sending"
-            />
-            <span
-              class="md-error"
-              v-if="!$v.form.courseDescription.required"
-            >The description is required</span>
-          </md-field>
-
-          <div class="md-layout-item">
-            <md-field :class="getValidationClass('abilityLevelId')">
-              <md-select
-                v-model="form.abilityLevelId"
-                name="abilityLevel"
-                id="ability-level"
-                placeholder="Ability Level"
-              >
-                <md-option
-                  v-for="abilityLevel in abilityLevels"
-                  :key="abilityLevel.abilityLevelId"
-                  :value="abilityLevel.abilityLevelId"
-                >{{ abilityLevel.description }}</md-option>
-              </md-select>
-              <span
-                class="md-error"
-                v-if="!$v.form.abilityLevelId.required"
-              >Please select an ability level</span>
-            </md-field>
-          </div>
-
-          <md-checkbox v-model="runCourse">I want to run this course</md-checkbox>
-          <div
-            id="host-details"
-            v-if="runCourse"
-          >
+          <md-card-content>
             <div class="md-layout md-gutter">
-              <div class="md-layout-item">
-                <md-field :class="getValidationClass('authorName')">
-                  <label for="author-name">Author Name</label>
+              <div class="md-layout-item md-small-size-100">
+                <md-field :class="getValidationClass('courseName')">
+                  <label for="course-name">Course Title</label>
                   <md-input
-                    name="authorName"
-                    id="author-name"
-                    v-model="form.authorName"
+                    name="courseName"
+                    id="course-name"
+                    v-model="form.courseName"
+                    :disabled="sending"
                   />
                   <span
                     class="md-error"
-                    v-if="!$v.form.authorName.required"
-                  >Your name is required</span>
-                </md-field>
-              </div>
-
-              <div class="md-layout-item">
-                <md-field :class="getValidationClass('authorRole')">
-                  <md-select
-                    v-model="form.authorRole"
-                    name="authorRole"
-                    id="author-role"
-                    placeholder="Author Role"
-                  >
-                    <md-option value="Product Developer">Product Developer</md-option>
-                    <md-option value="Product Analyst">Product Analyst</md-option>
-                  </md-select>
-                  <span
-                    class="md-error"
-                    v-if="!$v.form.authorRole.required"
-                  >Your role is required</span>
-                </md-field>
-              </div>
-
-              <div class="md-layout-item">
-                <md-field :class="getValidationClass('authorLevel')">
-                  <md-select
-                    v-model="form.authorLevel"
-                    name="authorLevel"
-                    id="author-level"
-                    placeholder="Author Level"
-                  >
-                    <md-option value="Level 1">Level 1</md-option>
-                    <md-option value="Level 2">Level 2</md-option>
-                    <md-option value="Level 3">Level 3</md-option>
-                    <md-option value="Level 4">Level 4</md-option>
-                    <md-option value="Level 5">Level 5</md-option>
-                  </md-select>
-                  <span
-                    class="md-error"
-                    v-if="!$v.form.authorLevel.required"
-                  >Your level is required</span>
+                    v-if="!$v.form.courseName.required"
+                  >A course title is required</span>
                 </md-field>
               </div>
             </div>
-          </div>
-        </md-card-content>
-        <md-progress-bar
-          md-mode="indeterminate"
-          v-if="sending"
-        />
-        <md-card-actions>
-          <md-button
-            type="submit"
-            class="md-primary"
-            v-on:click="submitForm"
-            :disabled="sending"
-          >Suggest Course</md-button>
-        </md-card-actions>
-      </md-card>
 
-      <md-snackbar :md-active.sync="suggestionAdded">
-        The suggestion "{{courseSuggestion}}" was successfully
-        submitted!
-      </md-snackbar>
+            <md-field :class="getValidationClass('courseDescription')">
+              <label for="course-description">Description</label>
+              <md-textarea
+                type="course-description"
+                name="courseDescription"
+                id="course-description"
+                v-model="form.courseDescription"
+                :disabled="sending"
+              />
+              <span
+                class="md-error"
+                v-if="!$v.form.courseDescription.required"
+              >The description is required</span>
+            </md-field>
+
+            <div class="md-layout-item">
+              <md-field :class="getValidationClass('abilityLevelId')">
+                <md-select
+                  v-model="form.abilityLevelId"
+                  name="abilityLevel"
+                  id="ability-level"
+                  placeholder="Ability Level"
+                >
+                  <md-option
+                    v-for="abilityLevel in abilityLevels"
+                    :key="abilityLevel.abilityLevelId"
+                    :value="abilityLevel.abilityLevelId"
+                  >{{ abilityLevel.description }}</md-option>
+                </md-select>
+                <span
+                  class="md-error"
+                  v-if="!$v.form.abilityLevelId.required"
+                >Please select an ability level</span>
+              </md-field>
+            </div>
+
+            <md-checkbox v-model="runCourse">I want to run this course</md-checkbox>
+            <div id="host-details" v-if="runCourse">
+              <div class="md-layout md-gutter">
+                <div class="md-layout-item">
+                  <md-field :class="getValidationClass('authorName')">
+                    <label for="author-name">Author Name</label>
+                    <md-input name="authorName" id="author-name" v-model="form.authorName"/>
+                    <span class="md-error" v-if="!$v.form.authorName.required">Your name is required</span>
+                  </md-field>
+                </div>
+
+                <div class="md-layout-item">
+                  <md-field :class="getValidationClass('authorRole')">
+                    <md-select
+                      v-model="form.authorRole"
+                      name="authorRole"
+                      id="author-role"
+                      placeholder="Author Role"
+                    >
+                      <md-option value="Product Developer">Product Developer</md-option>
+                      <md-option value="Product Analyst">Product Analyst</md-option>
+                    </md-select>
+                    <span class="md-error" v-if="!$v.form.authorRole.required">Your role is required</span>
+                  </md-field>
+                </div>
+
+                <div class="md-layout-item">
+                  <md-field :class="getValidationClass('authorLevel')">
+                    <md-select
+                      v-model="form.authorLevel"
+                      name="authorLevel"
+                      id="author-level"
+                      placeholder="Author Level"
+                    >
+                      <md-option value="Level 1">Level 1</md-option>
+                      <md-option value="Level 2">Level 2</md-option>
+                      <md-option value="Level 3">Level 3</md-option>
+                      <md-option value="Level 4">Level 4</md-option>
+                      <md-option value="Level 5">Level 5</md-option>
+                    </md-select>
+                    <span
+                      class="md-error"
+                      v-if="!$v.form.authorLevel.required"
+                    >Your level is required</span>
+                  </md-field>
+                </div>
+              </div>
+            </div>
+          </md-card-content>
+          <md-progress-bar md-mode="indeterminate" v-if="sending"/>
+          <md-card-actions>
+            <md-button
+              type="submit"
+              class="md-primary"
+              v-on:click="submitForm"
+              :disabled="sending"
+            >Suggest Course</md-button>
+          </md-card-actions>
+        </md-card>
+        </div>
+        <md-snackbar :md-active.sync="suggestionAdded">
+          The suggestion "{{courseSuggestion}}" was successfully
+          submitted!
+        </md-snackbar>
+      </div>
     </form>
   </div>
 </template>
@@ -270,5 +253,8 @@ export default {
   top: 0;
   right: 0;
   left: 0;
+}
+.center {
+  text-align: center;
 }
 </style>
